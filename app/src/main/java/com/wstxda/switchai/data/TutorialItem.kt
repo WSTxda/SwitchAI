0dc4beb822661f67a0566df6365cc1746ef76d32
WSTxda <wstxda@gmail.com>
2025-08-02 15:51:29 -0300

    Remove unused annotations and string resource

--- app/src/main/java/com/wstxda/switchai/data/TutorialItem.kt
+++ app/src/main/java/com/wstxda/switchai/data/TutorialItem.kt
@@ -1,11 +1,8 @@
 package com.wstxda.switchai.data
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
 data class TutorialItem(
-    @DrawableRes val iconRes: Int,
-    @DrawableRes val imageRes: Int,
-    @StringRes val titleRes: Int,
-    @StringRes val summaryRes: Int,
+    val iconRes: Int,
+    val imageRes: Int,
+    val titleRes: Int,
+    val summaryRes: Int,
 )

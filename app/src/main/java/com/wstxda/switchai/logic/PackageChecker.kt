692aefd330dcf48db6ca64771b6007c7769aeaff
WSTxda <wstxda@gmail.com>
2025-08-02 20:49:44 -0300

    Update package query permissions and checker logic

--- app/src/main/java/com/wstxda/switchai/logic/PackageChecker.kt
+++ app/src/main/java/com/wstxda/switchai/logic/PackageChecker.kt
@@ -2,10 +2,17 @@ package com.wstxda.switchai.logic
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 
 fun isPackageInstalled(context: Context, packageName: String): Boolean {
     return try {
-        context.packageManager.getPackageInfo(packageName, 0)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.packageManager.getPackageInfo(
+                packageName, PackageManager.PackageInfoFlags.of(0)
+            )
+        } else {
+            context.packageManager.getPackageInfo(packageName, 0)
+        }
         true
     } catch (_: PackageManager.NameNotFoundException) {
         false

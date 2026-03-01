fe285f8c36e8683886a999036543e56759c259da
WSTxda <wstxda@gmail.com>
2026-03-01 02:28:07 -0300

    Diff callback when subject

--- app/src/main/java/com/wstxda/switchai/ui/adapter/AssistantSelectorDiffCallback.kt
+++ app/src/main/java/com/wstxda/switchai/ui/adapter/AssistantSelectorDiffCallback.kt
@@ -5,13 +5,10 @@ import androidx.recyclerview.widget.DiffUtil
 class AssistantSelectorDiffCallback : DiffUtil.ItemCallback<AssistantSelectorRecyclerView>() {
     override fun areItemsTheSame(
         oldItem: AssistantSelectorRecyclerView, newItem: AssistantSelectorRecyclerView
-    ): Boolean = when {
-        oldItem is AssistantSelectorRecyclerView.AssistantSelector && newItem is AssistantSelectorRecyclerView.AssistantSelector -> oldItem.assistantItem.key == newItem.assistantItem.key
-
-        oldItem is AssistantSelectorRecyclerView.CategoryHeader && newItem is AssistantSelectorRecyclerView.CategoryHeader -> oldItem.title == newItem.title
-
-        oldItem is AssistantSelectorRecyclerView.ReorderTip && newItem is AssistantSelectorRecyclerView.ReorderTip -> true
-
+    ): Boolean = when (oldItem) {
+        is AssistantSelectorRecyclerView.AssistantSelector if newItem is AssistantSelectorRecyclerView.AssistantSelector -> oldItem.assistantItem.key == newItem.assistantItem.key
+        is AssistantSelectorRecyclerView.CategoryHeader if newItem is AssistantSelectorRecyclerView.CategoryHeader -> oldItem.title == newItem.title
+        is AssistantSelectorRecyclerView.ReorderTip if newItem is AssistantSelectorRecyclerView.ReorderTip -> true
         else -> false
     }
 
